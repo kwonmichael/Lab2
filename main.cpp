4045d188c468106c5ee1d5ce6a8ee1590086ce3c
rnjswodbs1101 <rnjswodbs1101@snu.ac.kr>
2026-04-01 17:48:28 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
-void func(int cnt) {
+void func(int start) {
+    
+    if (vec.size() == M) {
+        for (int i = 0; i < M; i++) {
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
 
-
-//modified by kwonjaeyun
-
-
-
-
+    for (int i = start; i <= N; i++) {
+        vec.push_back(i);    
+        func(i + 1);         
+        vec.pop_back();      
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    
+    func(1); 
+    
+    return 0;
 }
